56f27670f036b195c5adc98db19f50c9d1e65b70
root <chasenaidoo9@gmail.com>
2023-10-14 05:32:58 -0700

    Modified console

--- console.py
+++ console.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """The command interpreter"""
 import cmd
-import sys
 import shlex
 from models.base_model import BaseModel
 from models.user import User
@@ -12,36 +11,23 @@ from models.place import Place
 from models.review import Review
 from models import storage
 
-
 class HBNBCommand(cmd.Cmd):
     """Command processor"""
-    cmd.Cmd.completekey = None
+
     prompt = "(hbnb) "
 
-    def __init__(self):
-        super().__init__()
-        self.l_classes = {
-            'BaseModel': BaseModel,
-            'User': User,
-            'State': State,
-            'City': City,
-            'Amenity': Amenity,
-            'Place': Place,
-            'Review': Review
-        }
-
-    def preloop(self):
-        """Disables raw input to handle non-interactive input"""
-        if not sys.stdin.isatty():
-            self.use_rawinput = False
-        
-    def postloop(self):
-        """Cleanup or actions needed after the command loop has completed."""
-        if not sys.stdin.isatty():
-            print()
+    l_classes = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Place': Place,
+        'Review': Review
+    }
 
     def lookup_instance(self, args):
-        """Find and return an instance based on class name and ID"""
+        """Find and return an instance based on class name and ID."""
         if len(args) < 2:
             raise ValueError("** class name missing **")
 
@@ -58,7 +44,7 @@ class HBNBCommand(cmd.Cmd):
         raise ValueError("** no instance found **")
 
     def do_show(self, arg):
-        """Shows string representation of an instance passed"""
+        """Shows string representation of an instance passed."""
         try:
             args = arg.split()
             if len(args) == 0:
@@ -75,7 +61,7 @@ class HBNBCommand(cmd.Cmd):
             print(e)
 
     def do_destroy(self, arg):
-        """Deletes an instance passed"""
+        """Deletes an instance passed."""
         try:
             args = arg.split()
             instance = self.lookup_instance(args)
@@ -84,16 +70,16 @@ class HBNBCommand(cmd.Cmd):
                 if obj is instance:
                     key_to_delete = key
                     break
-            if key_to_delete:
-                del storage.all()[key_to_delete]
-                storage.save()
-            else:
-                raise ValueError("** no instance found **")
+                if key_to_delete:
+                    del storage.all()[key_to_delete]
+                    storage.save()
+                else:
+                    raise ValueError("** no instance found **")
         except ValueError as e:
             print(e)
 
     def do_create(self, type_model):
-        """Creates an instance according to a given class"""
+        """Creates an instance according to a given class."""
         try:
             if not type_model:
                 raise ValueError("** class name missing **")
@@ -126,8 +112,8 @@ class HBNBCommand(cmd.Cmd):
             print(e)
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and
-        id by adding or updating attribute (save the change into the JSON file)
+        """Updates an instance based on the class name and id by adding or
+        updating attribute (save the change into the JSON file).
         """
         try:
             if not arg:
@@ -156,26 +142,26 @@ class HBNBCommand(cmd.Cmd):
                     setattr(obj, args[2], args[3])
                     storage.save()
                     return
-            raise ValueError("** no instance found **")
+                raise ValueError("** no instance found **")
         except ValueError as e:
             print(e)
 
     def help_help(self):
-        """Prints help command description"""
-        print("Provides description of a given command")
+        """Prints a description of a given command."""
+        print("Provides a description of a given command")
 
     def emptyline(self):
-        """Do nothing when an empty line is entered"""
+        """Do nothing when an empty line is entered."""
         pass
 
     def do_quit(self, line):
-        """Quit command to exit the command interpreter"""
+        """Quit command to exit the command interpreter."""
         return True
 
     def do_EOF(self, line):
-        """EOF command to exit the command interpreter"""
+        """EOF command to exit the command interpreter."""
         return True
 
-
+    
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
